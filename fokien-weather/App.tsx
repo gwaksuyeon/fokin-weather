8574c81191f137b16b122b707de5a33998ad544e
gwaksuyeon <kwak7663@gmail.com>
2020-04-19 19:31:47 +0900

    app - API 설정

--- fokien-weather/App.tsx
+++ fokien-weather/App.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>순민이바보</Text>
-    </View>
-  );
-}
+import Loading from './Loading';
+import Weather from './Weather';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 12,
-    color: 'blue',
+const API_KEY = "96547a856d415e293a03c598085afc48";
+
+export default class extends React.Component {
+
+  state = {
+    isLoading: true,
+    temp: 0,
+    condition: '',
   }
-});
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { 
+        coords: { latitude, longitude } 
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you. so sad")
+    }
+  }
+
+  getWeather = async (latitude: number, longitude: number) => {
+    const { data: {
+      main: {temp},
+      weather
+    } } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+
+    this.setState({
+      isLoading: false,
+      temp,
+      condition: weather[0].main
+    })
+  }
+
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading/> : <Weather temp={Math.round(temp)} condition={condition}/>;
+  }
+}
